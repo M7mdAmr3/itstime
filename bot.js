090406121eeded4adba325f334d955acce30c777
M7mdAmr3 <43708078+M7mdAmr3@users.noreply.github.com>
2018-11-24 05:07:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,7 +57,7 @@ client.on('message', message => { // لا تغير شئ عشان ما تخرب �
 if (message.content === '.sp') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 20; x++) {
+      for(let x = 0; x < 40; x++) {
         message.channel.send(`MOHAMED AMR FOOOOOOOOOOOO2[ " ${x} " ]`)
           .then(m => {
             count++;
@@ -68,10 +68,10 @@ if (message.content === '.sp') {
 });
 
 client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '.sp') {
+if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 20; x++) {
+      for(let x = 0; x < 40; x++) {
         message.channel.send(`MOHAMED AMR FOOOOOOOOOOOO2[ " ${x} " ]`)
           .then(m => {
             count++;
