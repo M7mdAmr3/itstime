eea9165652e947dc6c71293692dd8ee4fcad26e1
M7mdAmr3 <43708078+M7mdAmr3@users.noreply.github.com>
2018-11-24 06:50:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,8 +57,8 @@ client.on('message', message => { // لا تغير شئ عشان ما تخرب �
 if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+      for(let x = 0; x < 0; x++) {
+        message.channel.send(`somq qmd hhh[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -71,8 +71,8 @@ client2.on('message', message => { // لا تغير شئ عشان ما تخرب 
 if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+      for(let x = 0; x < 0; x++) {
+        message.channel.send(`somq qmd hhh[ " ${x} " ]`)
           .then(m => {
             count++;
           })
