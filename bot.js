e2cfa40a81a5e9bd6315920804a3661c29dadc40
M7mdAmr3 <43708078+M7mdAmr3@users.noreply.github.com>
2018-11-25 07:20:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,7 +57,7 @@ client.on('message', message => { // لا تغير شئ عشان ما تخرب �
 if (message.content === '.sp') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 20; x++) {
+      for(let x = 0; x < 9000; x++) {
         message.channel.send(`Spamm For Crediiiiiiiiiit.....#[ " ${x} " ]`)
           .then(m => {
             count++;
@@ -71,7 +71,7 @@ client2.on('message', message => { // لا تغير شئ عشان ما تخرب 
 if (message.content === '.sp2') {
       let count = 0;
       let ecount = 0;
-      for(let x = 0; x < 20; x++) {
+      for(let x = 0; x < 9000; x++) {
         message.channel.send(`Spamm For Crediiiiiiiiiit.....#[ " ${x} " ]`)
           .then(m => {
             count++;
